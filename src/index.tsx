bb0f2b062adc6ead463b17b969da8111d4af91dc
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-04 23:15:43 +0700

    fixed gh page not working

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={"/moviepedia"}>
     <Provider store={store}>
       <App />
     </Provider>
