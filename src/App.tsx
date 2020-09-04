f5d37a3373cb51ba4c2923fd9ac4f5f2a7b90e0a
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-04 20:17:44 +0700

    added movie detalis page to mocyle

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,18 @@ import MainLayout from "./components/main-layout";
 
 import "./styles/global.scss";
 import { Route } from "react-router-dom";
+import MovieDetailsPage from "./pages/movie-details-page";
 
 const modules = [
   {
     name: "Home Page",
     Component: HomePage,
     url: "/",
+  },
+  {
+    name: "Movie Details",
+    Component: MovieDetailsPage,
+    url: "/details/:id",
   },
 ];
 
---
+++ src/pages/movie-details-page/movie-details-page.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieDetailsPage: React.FC = () => {
+  return <div>cok</div>;
+};
+
+export default MovieDetailsPage;
