6c2b290188975b209ed6c0f46d88d3d25f69ddef
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-04 16:44:31 +0700

    added moviecard component

---
+++ src/components/movie-card/movie-card.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieCard: React.FC = () => {
+  return <div className="movieCard"></div>;
+};
+
+export default MovieCard;
--- src/components/storefront/storefront.component.tsx
+++ src/components/storefront/storefront.component.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "./storefront.style.scss";
+import MovieCard from "../movie-card";
 
 const Storefront: React.FC = () => {
-  return <div className="storefront"></div>;
+  return (
+    <div className="storefront">
+      <MovieCard />
+    </div>
+  );
 };
 
 export default Storefront;
