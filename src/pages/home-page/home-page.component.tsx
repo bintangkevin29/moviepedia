1038796f7a5154a22ca5923b9e2266ea4d415941
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-04 16:23:34 +0700

    added popular component to home apge

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 
 import TopBanner from "../../components/top-banner";
+import Popular from "../../components/popular";
 
 const HomePage: React.FC = () => {
   return (
     <Fragment>
       <TopBanner />
+      <Popular />
     </Fragment>
   );
 };
