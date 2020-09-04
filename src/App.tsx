61ec6bd8822d97babd45e22af2b20d5a7968083a
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-04 14:07:24 +0700

    added main layout component to app ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+
 import HomePage from "./pages/home-page";
+import MainLayout from "./components/main-layout";
 
 import "./styles/global.scss";
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <MainLayout>
+      <HomePage />
+    </MainLayout>
+  );
 };
 
 export default App;
