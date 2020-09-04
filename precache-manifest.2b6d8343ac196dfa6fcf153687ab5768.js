4b9d8c2ac277672e11755e7e52f1960b8daa3faa
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-04 23:08:08 +0700

    Updates

---
+++ precache-manifest.2b6d8343ac196dfa6fcf153687ab5768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed933f290d622b9d558533f566d8cc31",
+    "url": "/moviepedia/index.html"
+  },
+  {
+    "revision": "b90b7ef6d1d42cf3d09c",
+    "url": "/moviepedia/static/css/main.67b29ef0.chunk.css"
+  },
+  {
+    "revision": "4af51dee2418e2045cb1",
+    "url": "/moviepedia/static/js/2.d3da48cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/moviepedia/static/js/2.d3da48cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90b7ef6d1d42cf3d09c",
+    "url": "/moviepedia/static/js/main.e6c79a3a.chunk.js"
+  },
+  {
+    "revision": "136f79611db1441e98d3",
+    "url": "/moviepedia/static/js/runtime-main.9656cc96.js"
+  },
+  {
+    "revision": "4f567b68faca97ae94dd422ca551c97b",
+    "url": "/moviepedia/static/media/banner.4f567b68.jpg"
+  }
+]);
