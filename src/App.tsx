7ce03e7ea33c4027dd4b166713b0bfa80ded8292
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-04 13:51:28 +0700

    changed app function to fat arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/home-page";
 
-function App() {
+const App = () => {
   return <HomePage />;
-}
+};
 
 export default App;
